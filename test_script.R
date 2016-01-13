8112314a81aeee0b62079a311889c26f398855b0
Aurelien Cottin <acottin@maupas.toulouse.inra.fr>
2016-01-13 18:04:53 +0100

    ajout premier fichier

---
+++ test_script.R
@@ -0,0 +1,17 @@
+#generer grille
+plot.new()
+plot(42,xlim=c(-10,10),ylim=c(-10,10))
+
+abline(v=(seq(-10,10,1)), col="lightgray", lty="dotted")
+abline(h=(seq(-10,10,1)), col="lightgray", lty="dotted")
+
+#entité
+t=xy.coords(0,0)
+tdir='d'
+
+testMur=function(t){
+  if(abs(t$x)>=11 | abs(t$y)>=11){
+    return(T)
+  }
+  return(F)
+}
