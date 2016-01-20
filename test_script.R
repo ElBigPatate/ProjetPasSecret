5a4295a04fcc08a24405e646c6bc54bd5be4e265
acottin <aurelien.cottin@gmail.com>
2016-01-20 10:28:45 +0100

    test boucle

--- test_script.R
+++ test_script.R
@@ -5,6 +5,7 @@ plot(42,xlim=c(-10,10),ylim=c(-10,10))
 abline(v=(seq(-10,10,1)), col="lightgray", lty="dotted")
 abline(h=(seq(-10,10,1)), col="lightgray", lty="dotted")
 
+
 #création matrice de direction
 dirVect=c("z","q","s","d")
 dirMatrix=matrix(rep(T,16),4,dimnames = list(dirVect,dirVect))
@@ -19,6 +20,20 @@ coor=matrix(c(0,-1,0,1,+1,0,-1,0), byrow=T, nrow=2, dimnames = list(c("x","y"), 
 #entité
 t=xy.coords(0,0)
 tdir='d'
+
+#--------------------------------------------------------------------------------------------------
+#Fonctions
+#--------------------------------------------------------------------------------------------------
+
+resetGrille=function(){
+  plot.new()
+  plot(42,xlim=c(-10,10),ylim=c(-10,10))
+  
+  abline(v=(seq(-10,10,1)), col="lightgray", lty="dotted")
+  abline(h=(seq(-10,10,1)), col="lightgray", lty="dotted")
+}
+
+
 
 testMur=function(t){
   if(abs(t$x)>=11 | abs(t$y)>=11){
@@ -40,4 +55,23 @@ move=function(direction){
   x=t$x+coor["x",direction]
   y=t$y+coor["y",direction]
   return(xy.coords(x,y))
+}
+
+
+
+#test mv
+for(i in seq(1,100)){
+  t=xy.coords(0,0)
+  tdir='d'
+  resetGrille()
+  while (! testMur(t)){
+    #bouger
+    t=move(tdir)
+    #afficher point
+    points(t)
+    #choisir direction
+    nouvelleDir=sample(dirVect,1)
+    #tester direction
+    tdir=testDir(tdir,nouvelleDir)
+  }
 }
